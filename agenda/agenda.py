81ba8a47c2f70f81bb4514511afdced62917975e
ari_monteiro <ari.monteiro006@gmail.com>
2026-01-08 09:39:01 -0300

    erro de sintaxe

--- agenda/agenda.py
+++ agenda/agenda.py
@@ -5,7 +5,7 @@ class Agenda:
         self.eventos = []
 
     def adicionar_evento(self, evento: Evento):
-        if not isinstance(evento, Evento);
+        if not isinstance(evento, Evento):
             raise TypeError("somente evento podem ser adicionados")
         
         self.eventos.append(evento)
